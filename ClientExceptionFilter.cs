2b77a9e53c39191252610ddb57bbf012be57ee26
MarinaStoliar <stoliarmarina39@gmail.com>
2021-08-26 11:33:11 +0300

    first commit

---
+++ ClientExceptionFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilterExeption
+{
+  
+    public class ClientExceptionFilter : Attribute, IExceptionFilter
+    {
+        private readonly ILogger<ClientExceptionFilter> logger;
+
+        public ClientExceptionFilter(ILogger<ClientExceptionFilter> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception != null)
+            {
+                logger.LogError(context.Exception, "Exception handled");
+                context.ExceptionHandled = true;
+            }
+
+            logger.LogInformation("Цей запит не може бути оброблено");
+        }
+    }
+}
---
+++ DateFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilterExeption
+{
+    public class DateFilterAttribute : ActionFilterAttribute
+    {
+        private DateTime _fromDt;
+        private DateTime _dtTo;
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _fromDt = Convert.ToDateTime(context.HttpContext.Request.Query["dtFrom"]);
+            _dtTo = Convert.ToDateTime(context.HttpContext.Request.Query["dtTo"]);
+
+            context.HttpContext.Response.StatusCode = 400;
+            var errorStr = $"{nameof(_fromDt)} < {nameof(_dtTo)}";
+            context.HttpContext.Response.Headers.Add("Error", new string[] { errorStr });
+        }
+    }
+}
